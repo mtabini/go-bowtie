d09bbd2199e284e7ef6ecbb9769554e6de490857
Marco Tabini <marcot@Marcobook-Pro.local>
2015-04-17 10:33:14 -0400

    - Docs

--- middleware/_init.go
+++
@@ -1,4 +0,0 @@
-// Package middleware contains several middlewares for bowtie.
-//
-// See https://github.com/mtabini/go-bowtie for more information.
-package middleware
--- middleware/router.go
+++ middleware/router.go
@@ -1,3 +1,6 @@
+// Package middleware contains several middlewares for bowtie.
+//
+// See https://github.com/mtabini/go-bowtie for more information.
 package middleware
 
 import (
