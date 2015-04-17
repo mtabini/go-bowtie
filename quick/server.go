ffab31c60ac0e9fee0dcfd1853e50095767a7bb0
Marco Tabini <marcot@Marcobook-Pro.local>
2015-04-17 10:24:08 -0400

    - Docs

--- quick/server.go
+++ quick/server.go
@@ -1,3 +1,7 @@
+// Package quick provides the easiest way to stand up a Bowtie server.
+//
+// The resulting server includes a plaintext logger, error handler, panic recovery, and
+// a router.
 package quick
 
 import (
@@ -5,11 +9,14 @@ import (
 	"github.com/mtabini/go-bowtie/middleware"
 )
 
+// Struct QuickServer encapsulates a Bowtie server and router. It can be passed
+// directly to net/http.ListenAndServe, and exposes all the router's methods.
 type QuickServer struct {
 	*bowtie.Server
 	*middleware.Router
 }
 
+// New creates a new QuickServer.
 func New() *QuickServer {
 	r := middleware.NewRouter()
 
--- quick/server_example_test.go
+++ quick/server_example_test.go
@@ -10,11 +10,13 @@ func Example_quick_server() {
 	// Create a new Bowtie quick server
 	s := New()
 
+	// Add a route
 	s.GET("/test/:id", func(c bowtie.Context) {
 		id := c.(*middleware.RouterContext).Params.ByName("id")
 
 		c.Response().WriteString("The ID is " + id)
 	})
 
+	// Serve data
 	http.ListenAndServe(":8000", s)
 }
